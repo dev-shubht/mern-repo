0ad02dbf341d7c9ca3480a203ac098a070275715
dev-shubht <webdevshubh@gmail.com>
2025-05-02 18:28:58 +0530

    changed db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URl);
+		const conn = await mongoose.connect(process.env.MONGO_URL);
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
